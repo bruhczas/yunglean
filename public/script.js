d558e68e532f6228898c04201fd3787c79287ead
bruhczas <71846288+bruhczas@users.noreply.github.com>
2025-01-06 19:55:42 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ chatForm.addEventListener('submit', async (e) => {
     chatInput.value = '';
 
     try {
-        const response = await fetch('http://localhost:3000/chat', {
+        const response = await fetch('/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
